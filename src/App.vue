c782b3e99eaba21878f1a3e1222496a2ef08c78b
YuliyaPriymak <bogatir.yuliya@gmail.com>
2021-01-18 13:38:48 +0200

    start, bind, events

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h1>{{message}}</h1>
+        <button v-on:click="inc">Click</button>
+        <span>{{counter}}</span>
+        <div v-on:mousemove="updateCoordinates">Coordinates: {{x}} - {{y}}</div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    data() {
+      return {
+        message: 'Hello World',
+        counter: 0,
+        x: 0,
+        y: 0,
+      }
+    },
+    methods: {
+      inc() {
+        this.counter++
+      },
+      updateCoordinates(event) {
+        this.x = event.clientX;
+        this.y = event.clientY;
+      }
+    }
+
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
