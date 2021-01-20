40baf79e2f46b2db5f2acdb60f3d9af8d020016b
YuliyaPriymak <bogatir.yuliya@gmail.com>
2021-01-20 13:10:24 +0200

    lesson 1 v-for, v-if, v-on

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,60 @@
         <h1>{{message}}</h1>
         <button v-on:click="inc">Click</button>
         <span>{{counter}}</span>
-        <div v-on:mousemove="updateCoordinates">Coordinates: {{x}} - {{y}}</div>
+        <p>{{counter > 10 ? 'Greater then 10' : 'Smaller then 10'}}</p>
+        <div v-on:mousemove="updateCoordinates">Coordinates: {{x}} - {{y}}
+            <span v-on:mousemove="dead">Dead spot</span>
+        </div>
+        <input type="text" v-on:keyup.enter="alert">
+        <div>
+            <input type="text" v-model="fullName.name"><br>
+            {{fullName.name}}<br>
+            <input type="text" v-model="fullName.surname"><br>
+            {{fullName.surname}}
+        </div>
+        <hr>
+        <div>
+            <button v-on:click="counter++">inc</button>
+            <button v-on:click="counter--">dec</button>
+            <button v-on:click="secondCounter++">dec</button>
+            {{counter}}|{{secondCounter}}
+            <p>{{result()}}|{{output}}</p>
+        </div>
+        <div>
+            <div class="demo" @click="attachedRed = !attachedRed"></div>
+            <div class="demo" :class="divClass"></div>
+            <div class="demo" :class="color"></div>
+            <input type="text" v-model="color">
+        </div>
+        <hr>
+        <div>
+            <p v-if="show">You can see this message</p>
+            <p v-else>Now you can see this message</p>
+            <button @click="show = !show">click</button>
+        </div>
+        <div>
+            <ul>
+                <li v-for="(el, i) in ingredients" :key="`${el}-${i}`">{{el}}</li>
+            </ul>
+        </div>
+        <hr>
+        <div>
+            <ul>
+                <li v-for="el in persons" :key="el.id">
+                    {{el.id}}. {{el.name}} age: {{el.age}}
+                </li>
+            </ul>
+        </div>
+        <hr>
+        <div>
+            <ul>
+                <li v-for="(el, i) in persons" :key="i">
+                    <div v-for="(value, key, index) in el" :key="index">
+                        {{key}}: {{value}}
+                    </div>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -15,8 +68,34 @@
       return {
         message: 'Hello World',
         counter: 0,
+        secondCounter: 0,
         x: 0,
         y: 0,
+        fullName: {
+          name: 'Yuliya',
+          surname: 'Priymak'
+        },
+        attachedRed: false,
+        color: 'green',
+        show: true,
+        ingredients: ['meat', 'fruits', 'apple'],
+        persons: [
+          {name: 'yulya', age: 33, id: 1},
+          {name: 'vera', age: 2, id: 2},
+
+        ]
+      }
+    },
+    computed: {
+      output() {
+        console.log('output');
+        return this.counter > 5 ? 'Greater then 5' : 'Smaller then 5'
+      },
+      divClass() {
+        return {
+          red: this.attachedRed,
+          blue: !this.attachedRed
+        }
       }
     },
     methods: {
@@ -26,6 +105,16 @@
       updateCoordinates(event) {
         this.x = event.clientX;
         this.y = event.clientY;
+      },
+      dead(event) {
+        event.stopPropagation();
+      },
+      alert() {
+        alert('alert!!!')
+      },
+      result() {
+        console.log('result');
+        return this.counter > 5 ? 'Greater then 5' : 'Smaller then 5'
       }
     }
 
@@ -40,5 +129,25 @@
         text-align: center;
         color: #2c3e50;
         margin-top: 60px;
+    }
+
+    .demo {
+        display: inline-block;
+        width: 100px;
+        height: 100px;
+        background-color: #000;
+        margin: 10px;
+    }
+
+    .red {
+        background-color: red;
+    }
+
+    .green {
+        background-color: green;
+    }
+
+    .blue {
+        background-color: blue;
     }
 </style>
