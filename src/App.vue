69aa69a72bb8397294867056a9e7e70deee26d26
YuliyaPriymak <bogatir.yuliya@gmail.com>
2021-02-12 23:19:49 +0200

    router start

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,6 @@
 <template>
     <div id="app">
-        <div>
-            <label>User name</label>
-            <input type="text" v-model="user.userName">
-        </div>
-        <div>
-            <label>User email</label>
-            <input type="text" v-model="user.email">
-        </div>
-        <button @click="submitData">post user</button>
-        <button @click="getUsers">get users</button>
-        <button @click="deleteUser">delete user</button>
-        <button @click="updateUser">update user</button>
-        <hr>
-        <ul>
-            <li v-for="(u, i) in users" :key="i">
-                {{u}}
-            </li>
-        </ul>
+
     </div>
 </template>
 
@@ -29,51 +12,12 @@
 
     data() {
       return {
-        user: {
-          userName: '',
-          email: ''
-        },
-        users: [],
-        url: 'https://vueproject-5776f-default-rtdb.firebaseio.com'
+
       }
     },
     computed: {},
     methods: {
-      submitData() {
-        this.$http.post(`${this.url}/vueProj.json`, this.user)
-        // this.$http.post('vueProj.json', this.user)
-          .then(res => {
-            console.log(res);
-          }, error => {
-            console.log(error);
-          })
-      },
 
-      getUsers() {
-        this.$http.get('https://vueproject-5776f-default-rtdb.firebaseio.com/vueProj.json')
-          .then((res) => {
-            return res.json()
-          }).then((res) => {
-          console.log(res);
-          Object.values(res).forEach(u => this.users.push(u))
-        })
-      },
-
-      deleteUser() {
-        this.$http.delete('https://vueproject-5776f-default-rtdb.firebaseio.com/vueProj/-MT0u23lv1azaNuo9OwA.json')
-          .then((res) => {
-            console.log(res, 'delete');
-          })
-      },
-
-      updateUser() {
-        this.$http.put('https://vueproject-5776f-default-rtdb.firebaseio.com/vueProj/-MT0vHBzWsN6O1rMyZk-.json', {
-          userName: 'name',
-          email: 'email'
-        }).then(res => {
-          console.log(res);
-        })
-      }
     }
 
   }
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource'
+import VueRouter from "vue-router";
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+Vue.use(VueRouter);
 Vue.use(VueResource);
 // Vue.http.options.root = 'https://vueproject-5776f-default-rtdb.firebaseio.com';
 Vue.config.productionTip = false;
